30009f6c80ef34a248e664efa2ba852f7951a0b2
my88899 <haojiutao@gmail.com>
2017-06-03 10:45:07 +0800

    R2_setDefultStartWorld

--- src/main/java/org/coach/tdd/template/Library.java
+++ src/main/java/org/coach/tdd/template/Library.java
@@ -1,7 +1,32 @@
 package org.coach.tdd.template;
 
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
+    private int [][] startMatric ;
+//    public boolean someLibraryMethod(){
+//        return true;
+//    }
+    public void creatStartMatric(int row, int col) {
+        startMatric = new int [row + 1][col + 1];
+        for (int i = 0; i < row + 1; i++) {
+            for (int j = 0; j < col + 1; j++) {
+                startMatric[i][j] = 0;
+            }
+        }
+    }
+    public int[][] returnMatric() {
+        return startMatric;
+    }
+
+    public void setDefaultLife(int i, int j) {
+        startMatric[i][j] = 1;
+    }
+
+    public void printToScream() {
+        for (int i = 0; i < startMatric.length; i++) {
+            for (int j = 0; j < startMatric[0].length; j++) {
+                System.out.print(startMatric[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/test/java/org/coach/tdd/template/LibraryTest.java
+++ src/test/java/org/coach/tdd/template/LibraryTest.java
@@ -1,13 +1,30 @@
 package org.coach.tdd.template;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
 public class LibraryTest {
+//    @Test
+//    public void shouldSomeLibraryMethodReturnTrue() {
+//        Library classUnderTest = new Library();
+//        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+//    }
     @Test
-    public void shouldSomeLibraryMethodReturnTrue() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    public void shouldReturnMatric() {
+        Library classUderTest = new Library();
+        int [][] testResult = new int [10][10];
+        int [][] defaultLife = {{4, 5}, {5, 5}, {5, 6}};
+        classUderTest.creatStartMatric(9, 9);
+        for (int i = 0; i < defaultLife.length; i++) {
+            classUderTest.setDefaultLife(defaultLife[i][0], defaultLife[i][1]);
+            testResult[defaultLife[i][0]][defaultLife[i][1]] = 1;
+            testResult[defaultLife[i][0]][defaultLife[i][1]] = 1;
+
+        }
+        classUderTest.printToScream();
+//        assertEquals();
+        assertArrayEquals(testResult, classUderTest.returnMatric());
     }
 }
+
